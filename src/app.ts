837e12afad3dbee861f5b8a02f79951c391342ab
bkuste2 <igorribeiro632@gmail.com>
2023-09-02 04:59:45 -0300

    :rocket: add ErrorMiddleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import cors = require("cors");
-import express from "express";
-import { router } from "./routes"
-import {errorMiddleware} from "./middlewares/error";
+import cors = require('cors')
+import express from 'express'
+import { router } from './routes'
+import { errorMiddleware } from './middlewares/error'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ type: 'application/vnd.api+json'}))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ type: 'application/vnd.api+json' }))
 
-app.use(router);
+app.use(router)
+
+app.use(errorMiddleware)
 
 export { app }
